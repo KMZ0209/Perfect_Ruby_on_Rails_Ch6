4fc584fcec24cf19a98054a382471624ce1ca60b
junohm410 <junohm410@gmail.com>
2023-12-22 14:44:55 +0900

    イベントキャンセル機能 / retirements用のディレクトリ作成

--- awesome_events/config/routes.rb
+++ awesome_events/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root "welcome#index"
-  get "/auth/:provider/callback" => "sessions#create"
-  delete "/logout" => "sessions#destroy", as: :logout
+  root 'welcome#index'
+  get '/auth/:provider/callback' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy', as: :logout
+
+  resource :retirements
 
   resources :events do
     resources :tickets
