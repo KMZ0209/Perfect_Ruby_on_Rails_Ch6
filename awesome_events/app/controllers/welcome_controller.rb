58c38d58feb64d0f4d4cc72b5e3bb4261d0e8e34
sharoa119 <sau.rin1109@gmail.com>
2024-01-29 16:05:55 +0900

    エラーハンドリング

--- awesome_events/app/controllers/welcome_controller.rb
+++ awesome_events/app/controllers/welcome_controller.rb
@@ -3,5 +3,6 @@ class WelcomeController < ApplicationController
 
   def index
     @events = Event.page(params[:page]).per(10).where("start_at > ?", Time.zone.now).order(:start_at)
+    raise StandardError
   end
 end
